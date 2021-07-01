afba150a8d1705f9d7f9a554651e44d1914c7060
jdhorne99 <86748433+jdhorne99@users.noreply.github.com>
2021-06-30 18:41:26 -0700

    Set temporary small ion valence constant to 2.0
    Changed the temporary value of the small ion valence to z=2.0 for the sake of testing the model with a valence other than 1.

--- src/math.jl
+++ src/math.jl
@@ -16,7 +16,7 @@ const b0 = sqrt(v0/(lB0/79.0))
 
 const lBbar = lB0/l0/79.0 # Reference value used as default
 
-const z = 1.0 # counterion valence, constant and =1 initially to test modifications
+const z = 2.0 # counterion valence, constant and =1 initially to test modifications
 
 #==============================================================================#
 # Mathematical functions
